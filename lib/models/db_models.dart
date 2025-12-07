aef4e4a1c968bcd57916dbc0ce3c84c0feefba91
ZHUREK GOAT <aktivlombard@Zhurek.local>
2025-12-07 17:04:17 +0500

    Hive models database

---
+++ lib/models/db_models.dart
@@ -0,0 +1,40 @@
+import 'package:hive/hive.dart';
+
+part 'db_models.g.dart';
+
+@HiveType(typeId: 0)
+class Product extends HiveObject {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String name;
+
+  @HiveField(2)
+  final double price;
+
+  @HiveField(3)
+  final String category;
+
+  @HiveField(4)
+  final String imagePath;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.category,
+    required this.imagePath,
+  });
+}
+
+@HiveType(typeId: 1)
+class Card extends HiveObject {
+  @HiveField(0)
+  final String cardNumber;
+
+  @HiveField(1)
+  final double balance;
+
+  Card({required this.cardNumber, required this.balance});
+}
---
+++ lib/models/db_models.g.dart
@@ -0,0 +1,90 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'db_models.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProductAdapter extends TypeAdapter<Product> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Product read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Product(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      price: fields[2] as double,
+      category: fields[3] as String,
+      imagePath: fields[4] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Product obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.price)
+      ..writeByte(3)
+      ..write(obj.category)
+      ..writeByte(4)
+      ..write(obj.imagePath);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class CardAdapter extends TypeAdapter<Card> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Card read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Card(
+      cardNumber: fields[0] as String,
+      balance: fields[1] as double,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Card obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.cardNumber)
+      ..writeByte(1)
+      ..write(obj.balance);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is CardAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
